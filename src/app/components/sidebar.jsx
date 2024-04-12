38017079c12baf788a1b0e1c8e1e7914af401479
raaghav03 <raghavnagpal2003@gmail.com>
2024-04-13 03:31:25 +0530

    basic sidebar

---
+++ src/app/components/sidebar.jsx
@@ -0,0 +1,10 @@
+export default function Sidebar() {
+  return (
+    <>
+      <div className="absolute inset-y-0 left-0 w-48 bg-stone-50 border-2 border-r-black  pl-12 ">
+    
+        Home
+      </div>
+    </>
+  );
+}
